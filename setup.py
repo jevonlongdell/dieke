42af81aa295a6d5b977f436c40a2ebb104626fa3
Jevon Longdell <jevon.longdell@gmail.com>
2018-05-29 12:29:02 +1200

    moved things around for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spectrapy",
+    version="0.0.1",
+    author="Jevon Longdell",
+    author_email="jevon.longdell@gmail.com",
+    description="Crystal field calculations for the rare earths",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/jevonlongdell/spectrapyt",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
