9caf1ebf816675dfcd906442492cd3cafe141c5a
Jevon Longdell <jevon.longdell@gmail.com>
2018-06-11 22:35:29 +1200

    started a script to debug zeeman matrix elements using simdiag and qutip

--- tests/testzeeman.py
+++ tests/testzeeman.py
@@ -1,6 +1,7 @@
 import pdb
 import dieke
 import numpy as np
+import qutip as q
 
 L = 1
 S = 0
@@ -55,4 +56,47 @@ for ii in range(2*J+1):
 
 
             
-Pr = dieke.RareEarthIon(2)
+#Pr = dieke.RareEarthIon(2)
+import qutip as q
+
+
+#consider two coupled spin L and S
+
+Lval = 1
+Sval = 0.5
+
+[Lx,Ly,Lz] = map(lambda x: q.tensor(x, q.qeye(2*Sval+1)),q.jmat(Lval))
+[Sx,Sy,Sz] = map(lambda x: q.tensor(q.qeye(2*Lval+1), x),q.jmat(Sval))
+
+Jx = Lx + Sx
+Jy = Ly + Sy
+Jz = Lz + Sz
+
+Lsq = Lx*Lx + Ly*Ly + Lz*Lz
+Ssq = Sx*Sx + Sy*Sy + Sz*Sz
+Jsq = Jx*Jx + Jy*Jy + Jz*Jz
+
+g = q.simdiag([Lsq,Ssq,Jsq,Jz])
+Jval = np.sqrt(4*g[0][2,:]+1)/2-0.5
+mJval = g[0][3,:]
+
+vects = g[1]
+
+Lz3 = 0*Lz
+Lz2 = 0*Lz
+for ii in range(len(vects)):
+    L = Lval
+    S = Sval
+    J = Jval[ii]
+    mJ = mJval[ii]
+    for jj in range(len(vects)):
+        Jp = Jval[jj]
+        mJp = mJval[jj]
+        matel1 = vects[ii].dag()*Lz*vects[jj]
+        # Using
+        
+        matel2 = sign * wignerlookup.w3j(2*J, 2, 2*Jp, -2*mJ, 0, -2*mJp) * \
+                                     dieke.reducedL(2*S, 2*L, 2*J, 2*S, 2*L, 2*Jp)
+        
+        if np.abs(matel1[0,0]-matel2)>1e-6:
+            raise Exception('spam', 'eggs')
