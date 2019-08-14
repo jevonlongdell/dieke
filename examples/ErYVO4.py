8874eff1d7330adbcaf60c5ceaba1372f25e2e9c
Jevon Longdell <jevon.longdell@gmail.com>
2019-08-14 15:25:18 -0400

    some more changes to ErYVO4 example

--- examples/ErYVO4.py
+++ examples/ErYVO4.py
@@ -3,12 +3,12 @@ import dieke
 import pickle
 import gzip
 import matplotlib.pyplot as plt
+import sys
 
 nf = 11  # 11 f-electrons means we're dealing with Er
 
-# This object contains the matricies we need for the
-# calculations all in the dictionary "FreeIonMatrix"
 
+#only compute the matrix elements the first run through
 try:
     f = gzip.open('Ermatricies.dat.gz', 'rb')
     print("Loading matricies")
@@ -38,24 +38,12 @@ for k in cfparams.keys():
 
 
 
-# cfparams['B20'] = 342
-# cfparams['B40'] = 109
-# cfparams['B60'] = -733
-# cfparams['B44'] = -695
-# cfparams['B64'] = -105
+cfparams['B22'] = 0
+cfparams['B42'] = 0
+cfparams['B62'] = 0
+cfparams['B66'] = 0
 
-# cfparams['B20'] = 308.2
-# cfparams['B40'] = 102.2
-# cfparams['B60'] = -735.7
-# cfparams['B44'] = -716.3
-# cfparams['B64'] = -25.5
-
-# cfparams['B20'] = 151.7
-# cfparams['B40'] = 19.9
-# cfparams['B60'] = -44.2
-# cfparams['B44'] = -921
-# cfparams['B64'] = -97
-
+     
 
 cfparams['B20'] = -206
 cfparams['B40'] = 362
@@ -63,13 +51,10 @@ cfparams['B44'] = 926
 cfparams['B60'] = -688
 cfparams['B64'] = 31.5
 
-
-
 # zeroify all cf params that are incompatible with YVOs symmetry
 H = H0
 for k, q in [(2, 0), (4, 0), (6, 0), (4, 4), (6, 4)]:
-        if 'B%d%d' % (k, q) in cfparams:
-            
+        if 'B%d%d' % (k, q) in cfparams:            
             if q == 0:
                 H = H+cfparams['B%d%d' % (k, q)]*Er.Cmatrix(k, q)
             else:
@@ -88,35 +73,85 @@ idx = np.argsort(evals)
 evals = evals[idx]
 E0 = evals[0] #ground state energy
 evals = evals - E0 #make everything relative to GS
-vec = vec[:, idx]
+vec = vec[:, idx] #sort eigenvalues
 
 
-Bvals = np.linspace(0,0.1,10)
-nrglevels = np.zeros([len(Bvals), 4+0*len(evals)])
+# Add a fixed energy to the hamiltonian to make the ground state
+# zero energy
+H = H - np.identity(len(evals))*E0
+
+
+
+
+
+Bvals = np.linspace(0,2.1,20)
+nrglevels = np.zeros([len(Bvals), len(evals)])
 
 muB = 0.46686 # Bohr magneton in wavenumbers per testla
 
+# angle magnetic field makes with Z access
+th = 0
+
+
+
+print('Calculating levels')
 for i, B in enumerate(Bvals):
+    print(i,end=" ")
+    sys.stdout.flush()
 #    cfparams2['ZETA'] = zeta
-    H = H0 +B*muB*(Er.FreeIonMatrix['Lz']+ 2*Er.FreeIonMatrix['Sz'])
+    H = H0 +B*muB*np.cos(th)*(Er.FreeIonMatrix['Lz']+ 2*Er.FreeIonMatrix['Sz'])+\
+        B*muB*np.sin(th)*(Er.FreeIonMatrix['Lx']+ 2*Er.FreeIonMatrix['Sx'])
     (evals, evects) = np.linalg.eig(H)
-    evals = np.sort(np.real(evals-E0))
-    evals = np.take(evals,[0,1,18,19])
+    evals = np.sort(np.real(evals))
+#    evals = np.take(evals,[0,1,18,19])
     nrglevels[i, :] = np.sort(np.real(evals-E0))
+print()
 
 
-nrglevels = nrglevels*29.98 #  to GHz
-F0 = 6589.0*29.98
+# nrglevels = nrglevels*29.98 #  to GHz
+# F0 = 6589.0*29.98
+
+
+nlevels = 30
+
+lines = np.zeros([len(Bvals),nlevels*(nlevels-1)])
+
+
+print('Calculating lines')
+count = 0
+for i in [0,1]: #range(nlevels-1): #lines out of the ground two states
+	for j in range(i+1,nlevels):
+		lines[:,count] = np.abs(nrglevels[:,i]-nrglevels[:,j])
+		count=count+1
+
+
+   
+#plt.ion()  # interactive plotting, so plt.show() doesn't block
+
+f1 = plt.figure(1)
+plt.title('Spectral lines around 1520nm')
+plt.axis([0, max(Bvals), 6584, 6594])
+plt.plot(Bvals, (lines))  #  , [zeta0, zeta0], [-10000, +50000])
+plt.xlabel('B field (T)')
+plt.ylabel('Energy (cm^-1)')
+plt.show()
 
 
 
-    
-plt.ion()  # interactive plotting, so plt.show() doesn't block
-#plt.axis([0, max(Bvals), -10, 7000])
-plt.plot(Bvals, nrglevels[:,3]-nrglevels[:,0] - F0,
-         Bvals, nrglevels[:,3]-nrglevels[:,1] - F0,
-         Bvals, nrglevels[:,2]-nrglevels[:,0] - F0,
-         Bvals, nrglevels[:,2]-nrglevels[:,1] - F0)  #  , [zeta0, zeta0], [-10000, +50000])
+f1 = plt.figure(2)
+plt.title('Energy Levels for Er:YVO4')
+plt.subplot(1,2,1)
+plt.axis([0, max(Bvals), -3,3])
+plt.plot(Bvals, nrglevels)  #  , [zeta0, zeta0], [-10000, +50000])
+plt.xlabel('B field (T)')
+plt.ylabel('Energy (cm^-1)')
+
+plt.subplot(1,2,2)
+plt.axis([0, max(Bvals), 6584,6594])
+plt.plot(Bvals, nrglevels)  #  , [zeta0, zeta0], [-10000, +50000])
+plt.xlabel('B field (T)')
+plt.ylabel('Energy (cm^-1)')
+
 plt.show()
 
 
